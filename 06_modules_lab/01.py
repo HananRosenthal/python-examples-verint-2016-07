37e995166e1306c2bd8b1127641913b419eb23dc
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-05 11:09:52 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,4 +1,15 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times
-"""
+##################Part 1
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print('Usage: %s <number of repetions>' % sys.argv[0])
+    sys.exit(1)
+
+(_, num) = sys.argv
+print('number of repetitions = {}'.format(num) )
+#print('{}'.format(type(num)))
+
+for i in range (0, int(num)):
+    print('Hello Python')
+
