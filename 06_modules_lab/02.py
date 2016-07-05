fda5c0ad0e774ff706ad7d99e399d7ba1dbd10a0
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-05 11:09:55 +0300

    Solution for: sol-python-verint.modules-lab/02.py

--- 06_modules_lab/02.py
+++ 06_modules_lab/02.py
@@ -1,4 +1,14 @@
-""" Write a program that reads 2 numbers from sys.argv
-and prints their sum.
-"""
+#####################Part 2
+import os
+import sys
+
+if len(sys.argv) != 3:
+    print('Usage: %s <num1>  <num2> ' % sys.argv[0])
+    sys.exit(1)
+
+(_, num1, num2) = sys.argv
+num1 = int(num1)
+num2 = int(num2)
+print('%d %d %d' % (num1, num2, num1+num2))
+
 
