5b22f44a431d57d35543d11150789e795ee863fe
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-06 16:43:03 +0300

    Solution for: sol-python-verint.syntax-lab/03.py

--- 05_syntax_lab/03.py
+++ 05_syntax_lab/03.py
@@ -1,4 +1,6 @@
 ################### Part 3
+from random import randint
+
 L = 10000
 r = randint(1, L)
 str_r = str(r)
@@ -6,7 +8,8 @@ print('The string number is {}'.format(str_r) )
 num_digits = len(str_r)
 print('The number %d contains %d digits' % (r,num_digits) )
 sum = 0
-for i in range(0, num_digits):
-    sum += int(str_r[i])
+for c in str_r:
+#for i in range(0, num_digits):
+    sum += int(c)
 
-print('The digits sum is {}'.format(sum) )
+print('The sum of digits {}'.format(sum) )
