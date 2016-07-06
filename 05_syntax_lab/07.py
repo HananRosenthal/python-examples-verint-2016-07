4338b46c0ddc22b72dcef42b76f967972b3d7862
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-06 16:43:18 +0300

    Solution for: sol-python-verint.syntax-lab/07.py

--- 05_syntax_lab/07.py
+++ 05_syntax_lab/07.py
@@ -1,15 +1,13 @@
 ################# Part 7
-while 1:
-    r1 = randint(1, 10)
+
+while True:
+    r1 = randint(1, 10) # a random number
     #print('{}'.format(r1))
-    print('Please gusee a number in the range 1 - 100')
-    r = int(input())
-    if r == r1:
+    guess = int(input('Please gusee a number in the range 1 - 100')) #The user guess
+    if guess == r1:
         print('BINGO')
         break
-    if r < r1:
-        print('your guess ({}) is too small'.format(r))
+    if guess < r1:
+        print('your guess ({}) is too small'.format(guess))
     else:
-        print('your guess ({}) is too big'.format(r))
-
-########################### End of Syntax exercise ############################
+        print('your guess ({}) is too big'.format(guess))
