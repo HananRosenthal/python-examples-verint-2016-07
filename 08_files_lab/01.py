7092add80c6e5a65355386170ae962e9841043a1
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-06 16:14:59 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,11 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
+import argparse
 
+parser = argparse.ArgumentParser(description='The program gets two files and adds the content of the first one to the end of the second file')
+parser.add_argument('-f1','--file1', help='file1 full path', required = True)
+parser.add_argument('-f2','--file2', help='file2 full path', required = True)
+kuku = parser.parse_args()
+
+with open(kuku.file1, 'r') as fin:
+    with open(kuku.file2, 'a') as fout:
+        for line in fin:
+            fout.write(line)
