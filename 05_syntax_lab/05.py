b30baaefe3baa0d44ff935374d35abcce9126b61
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-06 16:43:11 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,7 @@
 ####################### Part 5
-while 1:
+from random import randint
+
+while True:
     r = randint(1, 1000000)
     if (r % 7 == 0) and (r % 13 == 0) and (r % 15 == 0):
         print('{}'.format(r))
