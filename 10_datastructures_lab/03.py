00fdabc1616d0867d51247d4f25dd91607b87dd7
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-10 12:40:19 +0300

    Solution for: sol-python-verint.datastructures-lab/03.py

--- 10_datastructures_lab/03.py
+++ 10_datastructures_lab/03.py
@@ -1,5 +1,26 @@
-"""
-Define a list of 20 grades
-and print only the grades that
-are above average
-"""
+################Part 3
+import argparse
+
+parser = argparse.ArgumentParser(description='The program gets a host name from the user and search for it in a table')
+parser.add_argument('-p','--path', help='hosts file full path', required = True)
+kuku = parser.parse_args()
+
+hosts={} #dictionary declaration
+
+#build the dictionary
+with open(kuku.path, 'r') as f:
+    for line in f:
+        (device, address) = line.split("=")
+        hosts[device] = address
+
+#get a string from user and search for a match
+while True:
+    strr = input('Please enter a device name, enter to exit')
+    if strr == "":
+        break
+    elif strr in hosts:
+        print(hosts[strr])
+    else:
+        print('Device {} not found'.format(strr))
+
+print('Thank you and goodbye')
