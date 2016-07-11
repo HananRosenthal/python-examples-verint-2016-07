b17a9e31de8209b21bead4313e0e76a80b72d7c4
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-11 15:20:03 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,5 @@
-"""
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
-"""
 
+alphabet = [chr(x) for x in range(97,123)]
+duo = [x+y for x in alphabet for y in alphabet]
+res = [x+y for x in alphabet for y in duo]
+print(res)
