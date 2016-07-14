03ffc62a76dbd134064adc4811176bde7b291d4c
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-14 23:25:46 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,6 @@
-"""
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
 
+def get_params(times=3, text=None):
+    if type(text) is not str: raise Exception("Type of text")
+    if type(times) is not int: raise Exception("Type of times")
+
+get_params(125, 2)
