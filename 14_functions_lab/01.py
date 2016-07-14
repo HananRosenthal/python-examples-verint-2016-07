1f76cc571b253a545d5b608a420ff26f8ac0fb4a
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-14 23:10:29 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,18 @@
-"""
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
-"""
+from functools import reduce
+
+#function tha returns the sum of input parameters
+def mysum(*nums):
+    return sum([n for n in nums if type(n) == int])
+
+#function tha returns the product of input parameters
+def mymul(*nums):
+    list=[]
+    for number in nums:
+        if type(number) is int:
+            list.append(number)
+
+    return  reduce(lambda x, y: x*y, list)
+
+print('Sum= ', mysum('r', 1,2,'a','b'))
+print('Mul= ', mymul('t', 1,2,3,4,'s') )
 
