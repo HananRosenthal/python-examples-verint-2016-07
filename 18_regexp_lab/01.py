37ec1fce8a6ca7fd87ce38b2d904ee02499ef76b
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-20 14:22:53 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -1,28 +1,13 @@
 import argparse
 import string
 import re
-'''
-parser = argparse.ArgumentParser(description='Returns a parameter that match a given key')
-parser.add_argument('-p','--path', help='definitions file', required = True)
-parser.add_argument('-k','--key', help='key', required = True)
-kuku = parser.parse_args()
 
+#Finally:
 with open(kuku.path, 'r') as f:
     for line in f:
-        temp = line.split()
-        if temp[0].startswith('#'):
+        prefix = re.match(r'\s*#', line)
+        if prefix is not None:
             continue
-        elif temp[0] == kuku.key:
-            res = re.search("=\s+(\w*)", line)
-            print(res.group(1))
-'''
-#After change of attitude :)
-with open(kuku.path, 'r') as f:
-    for line in f:
-        temp = line.split()
-        if temp[0].startswith('#'):
-            continue
-        m = re.search(r'(\w+)\s*=\s*(\w*\s*\w*)', line)
+        m = re.search(r'(\w+)\s*=\s*(.*)', line)
         if m.group(1) == kuku.key and m is not None:
             print(m.group(1), '==>', m.group(2) )
-
