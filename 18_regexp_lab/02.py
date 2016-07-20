12834212debaa71bf17d96e75fd399042a98ee00
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-20 18:22:02 +0300

    Solution for: sol-python-verint.regexp-lab/02.py

--- 18_regexp_lab/02.py
+++ 18_regexp_lab/02.py
@@ -1,6 +1,25 @@
-"""
-Write a function toCamelCase(text) that returns the text camel cased
-Write a function to_underscore(text) that returns the text with 
-underscores between words
-"""
+import re
 
+def toCamelClass(value):
+    output = ""
+    Overfirstword = False
+    for word in value.split("_"):
+        if not word:
+            output += ""
+            continue
+        if Overfirstword:
+            output += word.capitalize()
+        else:
+            output += word.lower()
+            Overfirstword = True
+    return output
+
+def fromCamelCase(value):
+    return re.sub('([A-Z]+)', r'_\1', value).lower()
+
+
+test = '__hanan_rosenthal_The_second'
+res = toCamelClass(test)
+print( res)
+back = fromCamelCase(res)
+print(back)
