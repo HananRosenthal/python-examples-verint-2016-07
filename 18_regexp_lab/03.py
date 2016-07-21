6471e1ed48ab8c64d0f6f345e621d07cbaa63eb0
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-21 17:33:47 +0300

    Solution for: sol-python-verint.regexp-lab/03.py

--- 18_regexp_lab/03.py
+++ 18_regexp_lab/03.py
@@ -1,7 +1,6 @@
 import csv
 import argparse
 import string
-import re
 
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('-p','--path', help='csv file', required = True)
@@ -9,12 +8,7 @@ kuku = parser.parse_args()
 
 with open(kuku.path, 'r') as f:
     reader = csv.reader(f)
-    num_cols = len(next(reader))
-    f.seek(0 ,0)
-
     for row in reader:
-        if num_cols > 1:
+        if len(row) > 1:
             row[0],row[1] = row[1],row[0]
-            print(row)
-        else:#print as is...
-            print(row)
+        print(row)
