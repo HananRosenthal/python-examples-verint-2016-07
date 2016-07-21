43738b75a8252d2e50f0c262eaefc8c7411da970
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-21 10:40:04 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -2,12 +2,17 @@ import argparse
 import string
 import re
 
-#Finally:
+parser = argparse.ArgumentParser(description='Returns a parameter that match a given key')
+parser.add_argument('-p','--path', help='definitions file', required = True)
+parser.add_argument('-k','--key', help='key', required = True)
+kuku = parser.parse_args()
+
 with open(kuku.path, 'r') as f:
     for line in f:
         prefix = re.match(r'\s*#', line)
-        if prefix is not None:
+        if prefix:
             continue
         m = re.search(r'(\w+)\s*=\s*(.*)', line)
-        if m.group(1) == kuku.key and m is not None:
-            print(m.group(1), '==>', m.group(2) )
+        if m and m.group(1) == kuku.key:
+            mm=m.group(2).strip() #remove whitespaces at the end of whatever string is
+            print(m.group(1), '==>', mm )
