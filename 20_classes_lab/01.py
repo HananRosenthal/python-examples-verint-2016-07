2d63972af1767fde2647bc829c4a9164e6a486ec
HananRosenthal <hanan.rosenthal@verint.com>
2016-07-27 12:42:18 +0300

    Solution for: sol-python-verint.class-syntax-lab/01.py

--- 20_classes_lab/01.py
+++ 20_classes_lab/01.py
@@ -1,6 +1,28 @@
-"""
-The following code assumes a class
-named Summer exists. 
-Complete the class so correct result is printed
-"""
+##### Slass Synatx ##################
+##### Exercise-1 ##################
 
+class Summer(object):
+
+    def __init__(self):
+        self.val = 0
+
+    def add(self, *num):
+        self.val += sum([i for i in num])
+
+    def total(self):
+        return self.val
+
+
+
+s = Summer()
+t = Summer()
+
+s.add(10, 20)
+t.add(50)
+s.add(30)
+
+# should print 60
+print(s.total())
+
+# should print 50
+print(t.total())
